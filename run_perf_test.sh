8d9ae95c62959cf13af966dd10a78f3442b3b9c9
KiranWells <kiranwells1008@gmail.com>
2024-02-16 13:34:57 -0600

    improve performance testing script

--- run_perf_test.sh
+++ run_perf_test.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
 file=output/profiling/times_$(git log --oneline | head -n 1 | awk '{ print $1 }')_autogen.csv
+version=$1
+
+echo Preparing to test $version
+
+if [ ! -d output/profiling ]; then
+  mkdir -p output/profiling
+fi
 
 echo Building...
 
-cmake --build build/release --parallel -t performance
+cmake --build build/$version --parallel -t performance
 
 echo Testing...
 echo
 
-echo -e "scene count\tsimulation time\ttotal time" | tee $file
+echo -e "scene count,$version simulation time,$version total time" > $file
+echo -e "scene count\t$version simulation time\t$version total time"
 
 for sim_count in {10..82}
   do
     sim_count=$(calc 'int(2^('$sim_count'/5))' | awk '{print $1}')
-    ./build/release/tests/performance -m 0 -s $sim_count -a gp \
+    ./build/$version/tests/performance -m 0 -s $sim_count \
       | grep "Kernel took:" \
-      | awk '{print("'$sim_count', ",$3,", ",$6)}' \
+      | awk '{print("'$sim_count',",$3,",",$6)}' \
       | tee -a $file | awk -F ',' '{print $1,"\t",$2,"\t",$3}' \
       || (echo "Error! $sim_count"; exit 1)
   done
