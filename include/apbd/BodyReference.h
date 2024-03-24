666a0a96f8ea0c333accf3d9e831be41c6a58950
KiranWells <kiranwells1008@gmail.com>
2024-03-24 17:30:52 -0500

    Inline bodyreference and soa definitions into the header

--- include/apbd/BodyReference.h
+++ include/apbd/BodyReference.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "apbd/Body.h"
+#include "data/soa.h"
+#include "se3/lib.h"
 #include "util.h"
 
 namespace apbd {
@@ -170,5 +172,182 @@ public:
   IMPLEMENT_DELEGATED_FUNCTION(__host__ __device__ void write_state(),
                                data.write_state());
 };
+
+using Eigen::Vector3f, Eigen::Quaternionf;
+
+#define IMPLEMENT_READONLY_ACCESS_FUNCTIONS(AttributeType, RefType, Type,      \
+                                            attribute)                         \
+  inline AttributeType RefType::attribute() const {                            \
+    return data::global_store.Type.attribute.get(index);                       \
+  }
+#define IMPLEMENT_ACCESS_FUNCTIONS(AttributeType, RefType, Type, attribute)    \
+  inline AttributeType RefType::attribute() const {                            \
+    return data::global_store.Type.attribute.get(index);                       \
+  }                                                                            \
+  inline void RefType::attribute(AttributeType new_val) {                      \
+    data::global_store.Type.attribute.set(index, new_val);                     \
+  }
+
+IMPLEMENT_ACCESS_FUNCTIONS(vec7, BodyRigidReference, BodyRigid, xdotInit)
+IMPLEMENT_ACCESS_FUNCTIONS(Vector3f, BodyRigidReference, BodyRigid, position)
+IMPLEMENT_ACCESS_FUNCTIONS(Quaternionf, BodyRigidReference, BodyRigid, rotation)
+inline vec7 BodyRigidReference::x() const {
+  auto p = data::global_store.BodyRigid.position.get(index);
+  auto q = data::global_store.BodyRigid.rotation.get(index).coeffs();
+  return vec7(q(0), q(1), q(2), q(3), p(0), p(1), p(2));
+}
+IMPLEMENT_ACCESS_FUNCTIONS(vec7, BodyRigidReference, BodyRigid, x0)
+IMPLEMENT_ACCESS_FUNCTIONS(vec7, BodyRigidReference, BodyRigid, x1)
+inline void BodyRigidReference::x1(Eigen::Vector4f new_q,
+                                   Eigen::Vector3f new_p) {
+  data::global_store.BodyRigid.x1.set(index, vec7(new_q(0), new_q(1), new_q(2),
+                                                  new_q(3), new_p(0), new_p(1),
+                                                  new_p(2)));
+}
+IMPLEMENT_ACCESS_FUNCTIONS(Quaternionf, BodyRigidReference, BodyRigid, x1_0_rot)
+IMPLEMENT_ACCESS_FUNCTIONS(vec7, BodyRigidReference, BodyRigid, dxJacobi)
+inline void BodyRigidReference::dxJacobi(Eigen::Vector4f new_q,
+                                         Eigen::Vector3f new_p) {
+  data::global_store.BodyRigid.dxJacobi.set(
+      index, vec7(new_q(0), new_q(1), new_q(2), new_q(3), new_p(0), new_p(1),
+                  new_p(2)));
+}
+IMPLEMENT_ACCESS_FUNCTIONS(vec7, BodyRigidReference, BodyRigid, dxJacobiShock)
+inline void BodyRigidReference::dxJacobiShock(Eigen::Vector4f new_q,
+                                              Eigen::Vector3f new_p) {
+  data::global_store.BodyRigid.dxJacobiShock.set(
+      index, vec7(new_q(0), new_q(1), new_q(2), new_q(3), new_p(0), new_p(1),
+                  new_p(2)));
+}
+IMPLEMENT_READONLY_ACCESS_FUNCTIONS(bool, BodyRigidReference, BodyRigid,
+                                    collide)
+IMPLEMENT_READONLY_ACCESS_FUNCTIONS(float, BodyRigidReference, BodyRigid, mu)
+IMPLEMENT_READONLY_ACCESS_FUNCTIONS(Shape, BodyRigidReference, BodyRigid, shape)
+IMPLEMENT_READONLY_ACCESS_FUNCTIONS(float, BodyRigidReference, BodyRigid,
+                                    density)
+IMPLEMENT_ACCESS_FUNCTIONS(Vector3f, BodyRigidReference, BodyRigid, Mr)
+IMPLEMENT_ACCESS_FUNCTIONS(float, BodyRigidReference, BodyRigid, Mp)
+
+inline void BodyRigidReference::init(vec7 xInit) {
+  this->computeInertiaConst();
+  this->position(xInit.block<3, 1>(4, 0));
+  this->rotation(Quaternionf(xInit.block<4, 1>(0, 0)));
+  this->x0(xInit);
+}
+
+inline void BodyRigidReference::stepBDF1(unsigned int step,
+                                         unsigned int substep, float hs,
+                                         Eigen::Vector3f gravity) {
+  auto xdot = this->computeVelocity(step, substep, hs);
+  Eigen::Vector4f qdot = xdot.block<4, 1>(0, 0);
+  Eigen::Vector3f v = xdot.block<3, 1>(4, 0); // pdot
+  this->x0(this->x());
+  Eigen::Vector4f q = this->rotation().coeffs();
+  Eigen::Vector3f p = this->position();
+  auto w = se3::qdotToW(q, qdot); // angular velocity in body coords
+  Vector3f f = Vector3f::Zero();  // translational force in world space
+  Vector3f t = Vector3f::Zero();  // angular torque in body space
+  auto m = this->Mp();            // scalar mass
+  auto I = this->Mr();            // inertia in body space
+  Eigen::Vector3f Iw = I.array() * w.array(); // angular momentum in body space
+  f = f + m * gravity;                        // Gravity
+  t = t + se3::cross(Iw, w);                  // Coriolis
+  // Integrate velocities
+  w = w + hs * Eigen::Vector3f(t.array() / I.array());
+  v = v + hs * (f / m);
+  qdot = se3::wToQdot(q, w);
+  // Integrate positions
+  q = q + hs * qdot;
+  p = p + hs * v;
+  q = q / q.norm();
+  this->rotation(Eigen::Quaternionf(q));
+  this->position(p);
+  this->x1_0_rot(this->rotation());
+  this->x1(this->rotation().coeffs(), this->position());
+}
+
+inline void BodyRigidReference::clearShock() {
+  // TODO: sets layer to max
+}
+
+inline void BodyRigidReference::applyJacobiShock() {
+  this->x1(this->x1() + this->dxJacobiShock());
+  this->dxJacobiShock(vec7::Zero());
+}
+
+inline void BodyRigidReference::regularize() {
+  auto x1_ = this->x1();
+  this->position(x1_.block<3, 1>(4, 0));
+  Eigen::Vector4f q = x1_.block<4, 1>(0, 0);
+  q /= q.norm();
+  this->rotation(Eigen::Quaternionf(q));
+}
+
+inline bool BodyRigidReference::broadphaseGround(Eigen::Matrix4f Eg) {
+  Eigen::Matrix4f E = this->computeTransform();
+  return this->shape().broadphaseGround(E, Eg);
+}
+inline GroundNarrowphaseReturn
+BodyRigidReference::narrowphaseGround(Eigen::Matrix4f Eg) {
+  Eigen::Matrix4f E = this->computeTransform();
+  return this->shape().narrowphaseGround(E, Eg);
+}
+inline bool BodyRigidReference::broadphaseRigid(BodyRigidReference other) {
+  Eigen::Matrix4f E1 = this->computeTransform();
+  Eigen::Matrix4f E2 = other.computeTransform();
+  return this->shape().broadphaseShape(E1, other.shape(), E2);
+}
+inline NarrowphaseReturn
+BodyRigidReference::narrowphaseRigid(BodyRigidReference other) {
+  Eigen::Matrix4f E1 = this->computeTransform();
+  Eigen::Matrix4f E2 = other.computeTransform();
+  return this->shape().narrowphaseShape(E1, other.shape(), E2);
+}
+
+inline Eigen::Matrix4f BodyRigidReference::computeTransform() {
+  Eigen::Matrix4f E = Eigen::Matrix4f::Identity();
+  E.block<3, 3>(0, 0) = this->rotation().toRotationMatrix();
+  E.block<3, 1>(0, 3) = this->position();
+  return E;
+}
+
+inline vec7 BodyRigidReference::computeVelocity(unsigned int step,
+                                                unsigned int substep,
+                                                float hs) {
+  if (step == 0 && substep == 0)
+    return this->xdotInit();
+  else
+    return (this->x() - this->x0()) / hs;
+}
+inline void BodyRigidReference::computeInertiaConst() {
+  auto d = this->density();
+  auto s = this->shape();
+  auto I = s.computeInertia(d);
+  this->Mr(I.block<3, 1>(0, 0));
+  this->Mp(I(4));
+}
+
+inline Eigen::Vector3f BodyRigidReference::computePointVel(Eigen::Vector3f xl,
+                                                           float hs) {
+  // xdot = this.computeVelocity(k,ks,hs);
+  vec7 xdot = (this->x() - this->x0()) / hs;
+  Eigen::Vector4f qdot = xdot.block<4, 1>(0, 0);
+  Eigen::Vector3f pdot = xdot.block<3, 1>(4, 0); // in world coords
+  Eigen::Quaternionf q = this->rotation();
+  Eigen::Vector3f w =
+      se3::qdotToW(q.coeffs(), qdot); // angular velocity in body coords
+  // v = R*cross(w,xl) + pdot
+  return (q * w.cross(xl)) + pdot;
+}
+inline void BodyRigidReference::applyJacobi() {
+  this->x1(this->x1() + this->dxJacobi());
+  this->dxJacobi(vec7::Zero());
+  this->regularize();
+}
+inline void BodyRigidReference::write_state() {
+  auto r = rotation().coeffs();
+  printf("%f %f %f r %f %f %f %f", position()(0), position()(1), position()(2),
+         r(0), r(1), r(2), r(3));
+}
 
 } // namespace apbd
