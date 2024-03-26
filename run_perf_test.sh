5e6881ccea0525db300a595b649e97e1d99c6fc7
KiranWells <kiranwells1008@gmail.com>
2024-03-26 12:46:54 -0500

    Update benchmarking script

--- run_perf_test.sh
+++ run_perf_test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-file=output/profiling/times_$(git log --oneline | head -n 1 | awk '{ print $1 }')_autogen.csv
 version=$1
 
 echo Preparing to test $version
@@ -16,6 +15,10 @@ cmake --build build/$version --parallel -t performance
 echo Testing...
 echo
 
+for iter in {1..3}
+do
+  file=output/profiling/times_$(git log --oneline | head -n 1 | awk '{ print $1 }')_${version}_3x.${iter}.csv
+
 echo -e "scene count,$version simulation time,$version total time" > $file
 echo -e "scene count\t$version simulation time\t$version total time"
 
@@ -24,10 +27,15 @@ for sim_count in {10..82}
     sim_count=$(calc 'int(2^('$sim_count'/5))' | awk '{print $1}')
     ./build/$version/tests/performance -m 0 -s $sim_count \
       | grep "Kernel took:" \
-      | awk '{printf("'$sim_count',%s,%s\n",$3,$6)}' \
+      | awk '{printf("'$sim_count',%s,%s\n",$4,$7)}' \
       | tee -a $file | awk -F ',' '{print $1,"\t",$2,"\t",$3}' \
       || (echo "Error! $sim_count"; exit 1)
   done
+done
+
+cd output/profiling 
+
+python data_processing.py $(git log --oneline | head -n 1 | awk '{ print $1 }') ${version} 3x
 
 echo
 echo Done
